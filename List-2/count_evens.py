663300e377240dc4400400f202a51de51a5038c7
SleepNoMore <nninkovic.py@gmail.com>
2020-09-06 05:28:01 +0200

    Added List-2 Folder

---
+++ List-2/count_evens.py
@@ -0,0 +1,14 @@
+'''
+Return the number of even ints in the given array.
+'''
+
+def count_evens(nums):
+    even = 0
+    for num in nums:
+        if num % 2 == 0:
+            even += 1
+    return even
+
+# count_evens([2, 1, 2, 3, 4]) → 3
+# count_evens([2, 2, 0]) → 3
+# count_evens([1, 3, 5]) → 0
