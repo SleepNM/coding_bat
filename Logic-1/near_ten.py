2eda6dc5387a4c9d183571334f0540aacdcddb18
SleepNoMore <nninkovic.py@gmail.com>
2020-09-06 17:07:21 +0200

    Added Logic-1 Folder

---
+++ Logic-1/near_ten.py
@@ -0,0 +1,11 @@
+'''
+Given a non-negative number "num", return True if num is within 2 of a multiple of 10.
+'''
+
+def near_ten(num):
+    return  num % 10 < 3 or num % 10 > 7
+
+
+# near_ten(12) → True
+# near_ten(17) → False
+# near_ten(19) → True
